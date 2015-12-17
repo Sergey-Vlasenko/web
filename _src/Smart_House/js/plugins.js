95ca49fb740e8383fadad15d26ea4ee6cacaac5a
WerT <therazan7@gmail.com>
2015-12-17 02:09:41 +0200

    Smart_House v.0.2

--- _src/Smart_House/js/plugins.js
+++ _src/Smart_House/js/plugins.js
@@ -8,21 +8,50 @@ $(function(){
 $(window).scroll(function() {
 	if ($(this).scrollTop() > 90){
 		$('.head').addClass("white-block");
+		$('.logo__images').attr('src', 'img/logo.png');
+		// $('a#myLink').attr('href', 'http://www.example.com/');
+
 	}
 	else{
 		$('.head').removeClass("white-block");
+		$('.logo__images').attr('src', 'img/logo2.png');
 	}
 });
+//Слайдер галерея
+ $(function(){
+  $('.slider-gallery').bxSlider({
+    auto: false,
+    moveSlides: 1,
+    slideMargin:0,
+    nextText:'',
+    prevText:'',
+	pager:false,
+    controls: true,
+    autoReload: true,
+    breaks: [{screen:100, slides:1}, {screen:480, slides:1}, {screen:640, slides:1}, {screen:992, slides:1}, {screen:1200, slides:1}]
+  });
+ });
 
 //Галлерея
 $(function(){
    lightbox.option({
      'resizeDuration': 200,
      'showImageNumberLabel': false,
-     'wrapAround':false
+     'wrapAround':true
  });
 });
+// Scroll
+$(document).ready(function() {
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
 
+    };
+});
 /*функция, отображающая результат на странице */
 function changeResult(result){
 document.getElementById('result').innerHTML = result;
